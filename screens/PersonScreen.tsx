da753276b3753aa47cd89451402df61bc15a9697
MaksymBaturinskyi <89942450+MaksymBaturinskyi@users.noreply.github.com>
2024-03-15 20:58:39 +0200

    finished Person screen

--- screens/PersonScreen.tsx
+++ screens/PersonScreen.tsx
@@ -5,6 +5,7 @@ import {styles, theme} from "../theme/color";
 import {ChevronLeftIcon} from "react-native-heroicons/outline";
 import {HeartIcon} from "react-native-heroicons/solid";
 import {useNavigation} from "@react-navigation/native";
+import {MovieList} from "../components/movieList";
 
 const {width, height} = Dimensions.get('window');
 const ios = Platform.OS === 'ios';
@@ -13,6 +14,7 @@ const verticalMargin = ios ? '' : 'my-3';
 export const PersonScreen = () => {
     const navigation = useNavigation();
     const [isFavorite, setIsFavorite] = useState(false);
+    const [personMovies, setPersonMovies] = useState([1,2,3,4])
   return (
     <ScrollView className='flex-1 bg-neutral-900' contentContainerStyle={{paddingBottom: 20}}>
         <SafeAreaView
@@ -60,11 +62,38 @@ export const PersonScreen = () => {
                     />
                 </View>
             </View>
-            <View
-                className='mt-6'
-            >
-
+            <View className='mt-6'>
+                <Text className='text-white font-bold text-center text-3xl'>
+                    Keanu Reeves
+                </Text>
+                <Text className='text-base text-neutral-500 text-center'>
+                    London, United Kingdom
+                </Text>
             </View>
+            <View className='mx-3 p-4 flex-row mt-6 justify-between items-center bg-neutral-700 rounded-full'>
+                <View className='border-r-2 border-r-neutral-400 px-2 items-center'>
+                    <Text className='text-white font-semibold'>Gender</Text>
+                    <Text className='text-neutral-300 text-sm'>Male</Text>
+                </View>
+                <View className='border-r-2 border-r-neutral-400 px-2 items-center'>
+                    <Text className='text-white font-semibold'>Birthday</Text>
+                    <Text className='text-neutral-300 text-sm'>1054-95-33</Text>
+                </View>
+                <View className='border-r-2 border-r-neutral-400 px-2 items-center'>
+                    <Text className='text-white font-semibold'>Known for</Text>
+                    <Text className='text-neutral-300 text-sm'>Acting</Text>
+                </View>
+                <View className='px-2 items-center'>
+                    <Text className='text-white font-semibold'>Popularity</Text>
+                    <Text className='text-neutral-300 text-sm'>64.23</Text>
+                </View>
+            </View>
+            <View className='my-6 mx-4 space-y-2'>
+                <Text className='text-white text-lg'>Biography</Text>
+                <Text className='text-neutral-400 tracking-wide'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, illo.</Text>
+            </View>
+
+            <MovieList data={personMovies}  title={'Movies'} hideSeeAll={true}/>
         </View>
     </ScrollView>
   );
