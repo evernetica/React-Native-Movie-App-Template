1f0b9689b3ab3bca8b941cfa456d52d1f41bf059
MaksymBaturinskyi <89942450+MaksymBaturinskyi@users.noreply.github.com>
2024-03-19 17:36:09 +0200

    fixed loading

--- components/loading.tsx
+++ components/loading.tsx
@@ -6,7 +6,7 @@ import {theme} from "../theme/color";
 const {width, height} = Dimensions.get('window');
 export const Loading = () => {
   return (
-    <View style={{height, width}} className='absolute flex-row justify-center'>
+    <View style={{width, height}}  className='absolute justify-center items-center'>
         <Progress.CircleSnail thickness={12} size={160} color={theme.background} />
     </View>
   );
